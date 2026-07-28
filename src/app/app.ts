892e36cd234c0c65d0489ea5574cf19e21ad8c2e
ASM@GitHub2024 <asmaa14has@gmail.com>
2026-07-28 06:13:13 +0300

    start using PrimeNg select example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+
+interface Language {
+    label: string;
+    value: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SelectModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('e-commerce');
+  languages: Language[] = [
+    { label: 'English', value: 'en' },
+    { label: 'Deutsch', value: 'de' },
+    { label: 'Español', value: 'es' },
+    { label: 'Français', value: 'fr' },
+    { label: 'Italiano', value: 'it' },
+    { label: 'Türkçe', value: 'tr' },
+    { label: '日本語', value: 'ja' },
+    { label: '中文', value: 'zh' },
+  ];
+  selectedLanguage: string | undefined;
 }
